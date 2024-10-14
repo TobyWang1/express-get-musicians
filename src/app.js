e32a43e771f61bc3c4b970652334d542134641b1
TobyWang1 <fufummumm@gmail.com>
2024-10-14 14:51:36 +0100

    Add HTTP get method for musicians and band endpoints

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get('/musicians', async (request, response) => {
+    try {
+        const musicians = await Musician.findAll();
+        // Send the response with the data in JSON format
+        response.json(musicians);
+    } catch (error) {
+        response.status(500).json({ error: "Failed to retrieve musicians" });
+    }
+})
 
-
-
-
-
+app.get('/bands', async (request, response) => {
+    try {
+        const bands = await Band.findAll();
+        // Send the response with the data in JSON format
+        response.json(bands);
+    } catch (error) {
+        response.status(500).json({ error: "Failed to retrieve bands" });
+    }
+})
 
 module.exports = app;
